09fc4e02db5031a69126c3f2f88fb906adb325c5
Mike <a3804430@hotmail.com>
2020-11-30 15:51:10 +0800

    add interceptors

--- 08-Vuex_API/example/vuex-api-example/src/api/photoRequest.js
+++ 08-Vuex_API/example/vuex-api-example/src/api/photoRequest.js
@@ -4,4 +4,23 @@ const photoRequest = axios.create({
   baseURL: "https://vue-lessons-api.herokuapp.com/",
 });
 
+photoRequest.interceptors.request.use(
+  (config) => {
+    console.log("請求發起前");
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+photoRequest.interceptors.response.use(
+  (response) => {
+    console.log("請求發起後");
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export const getPhotoRequest = () => photoRequest.get("/photo/list");

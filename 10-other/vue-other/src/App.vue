79a813516c643a25e91439988dd6170d9e48e077
Mike <a3804430@hotmail.com>
2020-12-03 13:32:06 +0800

    add demo

---
+++ 10-other/vue-other/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const name = ref("mike");
+    return {
+      name,
+    };
+  },
+};
+</script>
+<template>
+  <h1 class="title">{{ name }}</h1>
+</template>
+
+<style lang="scss">
+.title {
+  font-size: 30px;
+  color: red;
+}
+</style>
